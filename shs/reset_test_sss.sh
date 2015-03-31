ff331ee2aa332762bd477840d51dcc05e5d93233
LiJiaJun <lijiajun@gmail.com>
2015-03-31 10:49:27 +0800

    mod tips

--- shs/reset_test_sss.sh
+++ shs/reset_test_sss.sh
@@ -30,23 +30,25 @@ send_mail() {
 		echo please input mail receiver.
 		exit
 	fi
-	
+		
 	mailContentFile=/tmp/chg_test_pswd_mail.txt
 	
-	echo "欢迎试用AlaSS测试账号，完整JSON配置信息："						>  $mailContentFile
-	echo ""																	>> $mailContentFile
-	cat  $ssCfgPath/$ssFileName 											>> $mailContentFile
-	echo ""																	>> $mailContentFile
-	echo "中英对照："														>> $mailContentFile
-	echo "　　服务器 IP  <=> server"										>> $mailContentFile
-	echo "　　服务器端口 <=> server_port"									>> $mailContentFile
-	echo "　　密码       <=> password"										>> $mailContentFile
-	echo "　　加密       <=> method"										>> $mailContentFile
-	echo "　　代理端口   <=> local_port"									>> $mailContentFile
-	echo ""																	>> $mailContentFile
-	echo "可参考ShadowSocks客户端使用教程: http://www.alafafa.com/?p=89"	>> $mailContentFile
-	echo ""																	>> $mailContentFile
-	echo "请尽快测试，测试账号密码在1小时后失效。"							>> $mailContentFile
+	echo "欢迎试用AlaSS测试账号，完整JSON配置信息："										>  $mailContentFile
+	echo ""																					>> $mailContentFile
+	cat  $ssCfgPath/$ssFileName 															>> $mailContentFile
+	echo ""																					>> $mailContentFile
+	echo "请尽快测试，测试账号密码在1小时后失效。"											>> $mailContentFile
+	echo ""																					>> $mailContentFile
+	echo "中英对照："																		>> $mailContentFile
+	echo "　　服务器 IP  <=> server"														>> $mailContentFile
+	echo "　　服务器端口 <=> server_port"													>> $mailContentFile
+	echo "　　密码       <=> password"														>> $mailContentFile
+	echo "　　加密       <=> method"														>> $mailContentFile
+	echo "　　代理端口   <=> local_port"													>> $mailContentFile
+	echo ""																					>> $mailContentFile
+	echo "配置方法可参考ShadowSocks客户端使用教程: http://www.alafafa.com/?p=89"			>> $mailContentFile
+	echo "如需下载ShadowSocks的Windows客户端，请加企鹅群:387477811，在群共享文件下载"		>> $mailContentFile
+	echo "测试满意之后，请通过淘宝购买: http://item.taobao.com/item.htm?id=42743439161"		>> $mailContentFile
 	
 	#cat $mailContentFile
 	mail -s "AlaSS Test Accout Info" $mailReceiver < $mailContentFile
