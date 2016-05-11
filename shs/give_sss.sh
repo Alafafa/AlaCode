2b0a03c997e1ecaa41644f8eafb200645b7d4dab
lijiajun@gmail.com <lijiajun@gmail.com>
2016-05-11 15:40:31 +0800

    fix host name bug

--- shs/give_sss.sh
+++ shs/give_sss.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo 
-ls -l $HOME/shadowsocks|grep flit+|sort -r -k 8|head -n 1|awk '{print $9}'|xargs cat 
+hostAbbr=`hostname | cut -d . -f 1`
+latestJson=`ls -l ~/shadowsocks|grep $hostAbbr+|sort -r -k 8|head -n 1|awk '{print $9}'`
+cat ~/shadowsocks/$latestJson
 echo && echo
