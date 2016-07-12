dd235bab32ed8e763254c9e9fa98094750e346be
lijiajun@gmail.com <lijiajun@gmail.com>
2016-07-13 02:12:12 +0800

    add qr_code info

--- shs/qr_code_sss.sh
+++ shs/qr_code_sss.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-echo 
 
 jsonFile=$1
+neatMode=$2
+echoPara=
+
+if [ "$neatMode" == "" ]; then
+	echo
+else
+	echoPara=-n 
+fi
 
 ssMethod=`~/maintain/bin/jq  -r '.method' $jsonFile`
 ssPswd=`~/maintain/bin/jq  -r '.password' $jsonFile`
@@ -9,6 +16,11 @@ ssHost=`~/maintain/bin/jq  -r '.server'   $jsonFile`
 ssPort=`~/maintain/bin/jq  -r '.server_port' $jsonFile`
 infoBase64=`echo "$ssMethod:$ssPswd@$ssHost:$ssPort"|base64`
 paraEncode=`echo -ne "ss://$infoBase64" | hexdump -v -e '/1 "%02x"' | sed 's/\(..\)/%\1/g'`
-echo "http://tool.oschina.net/action/qrcode/generate?output=image%2Fgif&error=L&type=0&margin=0&size=4&1466584587150&data=$paraEncode"
 
-echo && echo
+echo echo $echoPara "http://tool.oschina.net/action/qrcode/generate?output=image%2Fgif&error=L&type=0&margin=0&size=4&1466584587150&data=$paraEncode"
+
+echo $echoPara "http://tool.oschina.net/action/qrcode/generate?output=image%2Fgif&error=L&type=0&margin=0&size=4&1466584587150&data=$paraEncode"
+
+if [ "$neatMode" == "" ]; then
+	echo && echo
+fi
