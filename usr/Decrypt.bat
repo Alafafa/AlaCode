2d150b4a168f1eb2968c865c8999e39325e1f4e7
lijiajun@gmail.com <lijiajun@gmail.com>
2016-10-04 22:34:30 +0800

    purify Decrypt.bat

--- usr/Decrypt.bat
+++ usr/Decrypt.bat
@@ -1,12 +1,18 @@
 @echo off
+
+set szDateTime=%DATE:~0,2%%DATE:~3,2%%DATE:~6,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+
 if exist .\AlaSale.7z (
 	if exist .\AlaSale.xls (
 		echo AlaSale.xls file exists, rename it automatically!!!
-		if exist .\AlaSale.7z.old (
-			echo AlaSale.xls.old exists, delete it automatically!!!
-			del .\AlaSale.xls.old
-		)
-		rename .\AlaSale.xls .\AlaSale.xls.old
+		rem if exist .\AlaSale.7z.old (
+		rem	echo AlaSale.xls.old exists, delete it automatically!!!
+		rem	del .\AlaSale.xls.old
+		rem )
+		
+		echo rename AlaSale.xls AlaSale.%szDateTime%.xls
+
+		rename AlaSale.xls AlaSale.%szDateTime%.xls
 	)
 	7z x -p%alasPswdSub% .\AlaSale.7z
 )
