aa39097adcbf57b345b39137419e5fe702957ad3
lijiajun@gmail.com <lijiajun@gmail.com>
2016-10-04 17:16:20 +0800

    delete .xls file

--- usr/Decrypt.bat
+++ usr/Decrypt.bat
@@ -1,8 +1,12 @@
 @echo off
 if exist .\AlaSale.7z (
 	if exist .\AlaSale.xls (
-		echo AlaSale.xls file exists, delete it automatically!!!
-		del .\AlaSale.xls
+		echo AlaSale.xls file exists, rename it automatically!!!
+		if exist .\AlaSale.7z.old (
+			echo AlaSale.xls.old exists, delete it automatically!!!
+			del .\AlaSale.xls.old
+		)
+		rename .\AlaSale.xls .\AlaSale.xls.old
 	)
 	7z x -p%alasPswdSub% .\AlaSale.7z
 )
