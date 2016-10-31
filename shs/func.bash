f2517338c00b6912f4f88eb29cabb57fc9665bdb
lijiajun@gmail.com <lijiajun@gmail.com>
2016-10-31 19:42:42 +0800

    add bashrc setting assistant files

---
+++ shs/func.bash
@@ -0,0 +1,21 @@
+function getEnvVer {
+	#[[ -n $(jobs) ]] && echo -e "\e[7m"
+	echo $ENV_VER
+}
+
+function chenv {
+	export ENV_NOT_ECHO=1
+	chrel
+	unset ENV_NOT_ECHO
+	#echo 
+	#chver
+}
+
+function chver {
+	echo +------------------------------------------------------------------------------+
+	echo + 当前可用的OB_REL有：
+	echo +  1.[通用版本]
+	echo +  2.[多租户版本]
+	echo +------------------------------------------------------------------------------+
+	read -p 请输入你的选择\> envIdx
+}
---
+++ shs/userc.bash
@@ -0,0 +1,22 @@
+PS1='\[\e]0;\w\a\]\n\[\e[32m\]\u@\h#$(getEnvVer) \[\e[33m\]\w\[\e[0m\]\n\$ '
+
+export TZ='Asia/Shanghai'
+
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH"
+export LIBPATH="$LIBPATH"
+export SHLIB_PATH="$LIBPATH"
+export LD_LIBRARY_PATH="$LIBPATH"
+
+ulimit -c unlimited
+
+if [[ ${USER} == '' ]]; then
+	export USER='Sindo'
+fi
+
+if [[ ${ENV_VER} == '' ]]; then
+	banner "I am "$USER
+	
+	echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	echo !!!!!!!!!!!!!!!!!!!!!!! Warmly welcome to use Alafafa console !!!!!!!!!!!!!!!!!!
+	echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+fi
